376f97a33843937e7ac2576515424be49b832b8f
BT_2N2_04 <sample@bths.edu>
2023-09-28 14:01:41 -0400

    Added point class

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,6 +1,6 @@
 public class RectangleRunner {
     public static void main(String[] args){
-        Rectangle plot1 = new Rectangle(150,200);
+      /*  Rectangle plot1 = new Rectangle(150,200);
         Rectangle plot2 = new Rectangle(125);
         Rectangle plot3 = new Rectangle();
 
@@ -14,6 +14,20 @@ public class RectangleRunner {
         int area3 = plot3.calculateArea();
 
         System.out.println("These three plots requires " + (area1 + area2 + area3) + " square feet of seed");
+        int len = 65;
+        int wid = len + 10;
+        Rectangle rect = new Rectangle(len, wid);
 
+        Rectangle rect = new Rectangle(10.0, 30.0);
+
+        Rectangle rect1 = new Rectangle(0);
+
+
+        int len = 65;
+        int wid = len + 10;
+        Rectangle rect1 = new Rectangle(len, wid);
+        Rectangle rect2 = new Rectangle(10.0, 30.0);
+        Rectangle rect3 = new Rectangle(0);
+*/
     }
 }
